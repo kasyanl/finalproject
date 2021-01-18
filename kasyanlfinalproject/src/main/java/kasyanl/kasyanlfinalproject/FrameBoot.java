7e3c71a56661644925cea27896df7c77486d9720
Kasyanl <1784158@tut.by>
2021-01-18 17:56:07 +0300

    first

---
+++ kasyanlfinalproject/src/main/java/kasyanl/kasyanlfinalproject/FrameBoot.java
@@ -0,0 +1,23 @@
+package kasyanl.kasyanlfinalproject;
+
+
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+public class FrameBoot extends JFrame{
+    public static void createGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Test frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Test label");
+        frame.getContentPane().add(label);
+
+        frame.setPreferredSize(new Dimension(200, 100));
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
---
+++ kasyanlfinalproject/src/main/java/kasyanl/kasyanlfinalproject/KasyanlfinalprojectApplication.java
@@ -0,0 +1,17 @@
+package kasyanl.kasyanlfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+
+@SpringBootApplication
+public class KasyanlfinalprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KasyanlfinalprojectApplication.class, args);
+
+
+    }
+
+}
